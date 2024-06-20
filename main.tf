363849b17eee039a53019e8b243183143721ae79
Sachin <sachinbaswar@gmail.com>
2024-06-20 07:49:06 +0530

    added bastion subnet code in main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,10 @@ resource "azurerm_subnet" "subnet2" {
   address_prefixes = ["10.0.2.0/24"]
   
 }
+resource "azurerm_subnet" "bastion-host" {
+  name = "AzureBastionSubnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = ["10.0.4.0/26"]
+  
+}
