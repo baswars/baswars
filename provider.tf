fd42985dab2863975568eda890f9b862af28c66b
Sachin Baswar <140378657+baswars@users.noreply.github.com>
2026-01-28 13:56:24 +0530

    Update azurerm provider and configure backend
    Updated azurerm provider version and added backend configuration.

--- provider.tf
+++ provider.tf
@@ -1,14 +1,20 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">3"
+      source = "hashicorp/azurerm"
+      version = "4.58.0"
     }
   }
-}
-provider "azurerm" {
-  features {
+ backend "azurerm" {
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
 
   }
+}
 
+provider "azurerm" {
+  feature {}
+  Subscription_id = "e05d12f7-07a5-4237-b6f6-c1e092965136"
 }
